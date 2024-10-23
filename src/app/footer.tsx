6a7c0a74d50fffd198c749b5a3a073809f0d8095
Max Fonda <mxwll155@gmail.com>
2024-10-23 19:17:09 -0400

    updated git link

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <div className="w-screen h-12 flex justify-between">
       <div className="fixed flex flex-row justify-center items-center">
-        <Link href="https://github.com/phl0at/next-bg3b">
+        <Link href="https://github.com/phl0at/bg3b-next">
           <DiGithubBadge
             className="text-blue-300 hover:text-blue-500 hover:-translate-y-1 transition ease-in-out delay-250 duration-300"
             size="55"
