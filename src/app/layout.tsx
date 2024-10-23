901539b32fcfc1fb33b9d7f2bc21098fed5ff9d5
Max Fonda <mxwll155@gmail.com>
2024-10-23 18:48:23 -0400

    rem font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import Header from "@/app/header";
 import Footer from "@/app/footer";
 import { MenuProvider } from "@/context/menu";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "BG3Builds",
@@ -29,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-900 flex flex-col justify-between h-screen`}
+        className={`antialiased bg-zinc-900 flex flex-col justify-between h-screen`}
       >
         <MenuProvider>
           <Header />
