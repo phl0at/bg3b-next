4d03907082766bbc0f1b215baad3b0f4d84a199c
Max Fonda <mxwll155@gmail.com>
2025-01-31 16:34:34 -0500

    changed revalidation rules on next cache

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,6 +4,7 @@ const key = process.env.API_KEY;
 export async function getData(path: string) {
   const res = await fetch(`${url}/rest/${path}`, {
     headers: { "X-API-KEY": `${key}` },
+    next: { revalidate: 3600 },
   });
   const data = await res.json();
   return data.data;
