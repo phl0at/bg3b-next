858a5efc065c0dc676229c80894441dfa004fadb
Max Fonda <mxwll155@gmail.com>
2025-04-12 19:41:39 -0400

    loading on main page

--- src/app/build/[id]/origins/loading.tsx
+++ src/app/build/[id]/origins/loading.tsx
@@ -1,11 +1,11 @@
 "use client";
 import React from "react";
-import { FadeLoader } from "react-spinners";
+import { SyncLoader } from "react-spinners";
 
 const Loading = () => {
   return (
     <main className="h-screen w-screen flex items-center justify-center">
-      <FadeLoader />;
+      <SyncLoader />;
     </main>
   );
 };
