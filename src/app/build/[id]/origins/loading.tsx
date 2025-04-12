39814eaca55e5e09826682b9c1f82d3db5fab800
Max Fonda <mxwll155@gmail.com>
2025-04-12 19:09:50 -0400

    loading on main page

--- src/app/build/[id]/origins/loading.tsx
+++ src/app/build/[id]/origins/loading.tsx
@@ -1,11 +1,12 @@
+"use client";
 import React from "react";
 import { MoonLoader } from "react-spinners";
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center">
+    <main className="h-screen w-screen flex items-center justify-center">
       <MoonLoader />;
-    </div>
+    </main>
   );
 };
 
