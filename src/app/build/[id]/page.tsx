dfce160c8cc1fcc5c7b3e134ed30c88f946219ad
Max Fonda <mxwll155@gmail.com>
2025-06-14 18:52:45 -0400

    rebuilding main page

---
+++ src/app/build/[id]/page.tsx
@@ -0,0 +1,36 @@
+import Equipment from "@/components/equipment";
+import React from "react";
+
+const BuildPage = () => {
+  return (
+    <main className="h-screen flex gap-4 items-center justify-center mr-[9.85%]">
+      <div
+        className="
+        text-lg
+        bg-opacity-80
+        bg-stone-950
+        outline-amber-400
+        outline
+        shadow-black
+        shadow-2xl
+        w-[40%]
+        max-w-[760px]
+        min-w-[440px]
+        h-[93%]
+        max-h-[1080px]
+        min-h-[615px]
+        rounded-3xl
+        flex
+        flex-col
+        justify-start
+        items-center
+        py-12"
+      >
+      React State Changes Here
+      </div>
+      <Equipment/>
+    </main>
+  );
+};
+
+export default BuildPage;
