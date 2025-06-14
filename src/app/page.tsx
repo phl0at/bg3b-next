1f1002e3fe7025e9fe908c76d6ff1423377b65f5
Max Fonda <mxwll155@gmail.com>
2025-06-14 19:09:32 -0400

    build menu store items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
     <main className="w-full h-screen flex justify-center items-center">
       <ul className="flex flex-row gap-20">
         <li className="font-sans text-6xl text-amber-100 hover:text-amber-300 hover:shadow-sm hover:-translate-y-1 transition ease-in-out delay-250 duration-300">
-          <Link href="/build/new/origins" className="flex flex-col items-center gap-2 ">
+          <Link href="/new-build" className="flex flex-col items-center gap-2 ">
             <FaCirclePlus />
             New
           </Link>
