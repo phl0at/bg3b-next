d9f64836d5a3aa9d5551a8d0c6b984d7adcf6244
Max Fonda <mxwll155@gmail.com>
2025-07-01 14:34:07 -0400

    helmets added to lib

---
+++ src/lib/cloaks.js
@@ -0,0 +1,54 @@
+const Cloaks = [
+  {
+    description: 'Description: Grants a +1 bonus to Armor Class and Saving Throw.',
+    id: 1,
+    img: 'Cloak of Protection',
+    modifiers: 'armour_class+1',
+    name: 'Cloak of Protection',
+    rarity: 'Uncommon',
+    spell: null,
+    type: 'Clothing'
+  },
+  {
+    description: 'The Shadow Itself: Once per turn when you kill an enemy, shroud yourself in primaeval darkness to become Invisible for 2 turns.',
+    id: 2,
+    img: 'The Deathstalker Mantle',
+    modifiers: null,
+    name: 'The Deathstalker Mantle',
+    rarity: 'Rare',
+    spell: null,
+    type: 'Clothing'
+  },
+  {
+    description: 'Stealthy Critical: While Hiding, the number you need to roll a Critical Hit while attacking is reduced by 1. This effect can stack.',
+    id: 3,
+    img: 'Shade Slayer Cloak',
+    modifiers: null,
+    name: 'Shade Slayer Cloak',
+    rarity: 'Very Rare',
+    spell: null,
+    type: 'Clothing'
+  },
+  {
+    description: 'Flaming Shroud: A creature that damages the wearer within 2m / 7ft receives Burning.',
+    id: 4,
+    img: 'Cindermoth Cloak',
+    modifiers: null,
+    name: 'Cindermoth Cloak',
+    rarity: 'Uncommon',
+    spell: null,
+    type: 'Clothing'
+  },
+  {
+    description: 'Description: The wearer is perpetually just a little bit damp.',
+    id: 5,
+    img: 'Reverse Rain Cloak',
+    modifiers: null,
+    name: 'Reverse Rain Cloak',
+    rarity: 'Uncommon',
+    spell: null,
+    type: 'Clothing'
+  }
+]
+
+export default Cloaks
