45b9db3f5d039a424668016caf5a931d2a3bef48
Max Fonda <mxwll155@gmail.com>
2025-07-01 14:39:20 -0400

    added boots to lib

---
+++ src/lib/boots.js
@@ -0,0 +1,54 @@
+const Boots = [
+  {
+    description: 'Description: Shallow, web-like lines have been tooled into these boiled leather boots.',
+    id: 1,
+    img: 'Drow Leather Boots',
+    modifiers: null,
+    name: 'Drow Leather Boots',
+    rarity: 'Common',
+    spell: null,
+    type: 'Clothing'
+  },
+  {
+    description: 'Description: Peals of thunder rumble at the back of your skull with every step taken in these boots.&*&Arcane Echomalefaction: When the wearer inflicts a condition upon a hostile creature, they also inflict 2 turns of Reverberation.',
+    id: 2,
+    img: 'Boots of Stormy Clamour',
+    modifiers: null,
+    name: 'Boots of Stormy Clamour',
+    rarity: 'Uncommon',
+    spell: null,
+    type: 'Clothing'
+  },
+  {
+    description: 'Description: Scratched in places, scorched in others, and worn thin at the heel, these boots bear the indelible marks of experience. &*&        Shadow Teleportation: Teleport to an unoccupied, obscured spot.',
+    id: 3,
+    img: 'Dark Justiciar Boots',
+    modifiers: null,
+    name: 'Dark Justiciar Boots',
+    rarity: 'Rare',
+    spell: 'Shadow Teleportation',
+    type: 'Medium'
+  },
+  {
+    description: "Description: Darker than a starless night, Night Walkers are made for secrecy in the honour of Shar. This particular pair was found deep in the ruins of an Underdark fortress. Though the leather is old and the magic is fading, the boots were recently cleaned and oiled with care.&*&        Night Walker: Can't be Enwebbed, Entangled, or Ensnared and can't slip on Grease or Ice.",
+    id: 4,
+    img: 'Disintegrating Night Walkers',
+    modifiers: null,
+    name: 'Disintegrating Night Walkers',
+    rarity: 'Very Rare',
+    spell: 'Misty Step',
+    type: 'Clothing'
+  },
+  {
+    description: "Description: 'If I had a copper for all the adventurin folk I seen gutted or cut up, or clawed like they was made of wet paper, just cause they couldn't move quick enough when speed might have saved em... well, I'd be suckin good brandy from crystal rather than this fuckin swill.' - Overheard in a tavern in Baldur's Gate.&*&        Legendary Mobility: You gain Freedom of Movement and Longstrider.",
+    id: 5,
+    img: 'Boots of Persistence',
+    modifiers: null,
+    name: 'Boots of Persistence',
+    rarity: 'Very Rare',
+    spell: 'Freedom of Movement&*&Longstrider',
+    type: 'Medium'
+  }
+]
+
+export default Boots
