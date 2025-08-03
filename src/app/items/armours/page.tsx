b474ad03e1388d678513964d3ef248728238261a
Max Fonda <mxwll155@gmail.com>
2025-08-02 21:11:53 -0400

    modifying state/static data access

--- src/app/items/armours/page.tsx
+++ src/app/items/armours/page.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ItemList from "@/components/equipmentMenu/itemList";
+import Armours from "@/lib/armours";
 
 const ArmoursPage = () => {
-  return <ItemList slot="armour" title="Armours" />;
+  return <ItemList items={Armours} slot="armour" title="Armours" />;
 };
 
 export default ArmoursPage;
