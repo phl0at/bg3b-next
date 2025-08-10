94ef32ab30e850ced4640aba4e193af1429902b8
Max Fonda <mxwll155@gmail.com>
2025-08-09 20:47:13 -0400

    equipment component

--- src/app/items/rings/ring-2/page.tsx
+++ src/app/items/rings/ring-2/page.tsx
@@ -3,7 +3,7 @@ import ItemList from "@/components/equipmentMenu/itemList";
 import Rings from "@/lib/rings";
 
 const RingsPage = () => {
-  return <ItemList items={Rings} slot="ring1" title="Rings" />;
+  return <ItemList items={Rings} slot="ring2" title="Rings" />;
 };
 
 export default RingsPage;
